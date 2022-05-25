d9033e6d890223c8376ed0eecfd6922b0e91c157
Egor8kuznetsov <egor.0894@gmail.com>
2022-05-25 20:45:06 +0300

    Initial commit

---
+++ src/test/java/Hooks/WebHooks.java
@@ -0,0 +1,23 @@
+package Hooks;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.qameta.allure.selenide.AllureSelenide;
+
+public class WebHooks {
+    @Before
+    public static void setWebDriverFormat() {
+        Configuration.startMaximized = true;
+        SelenideLogger.addListener("AllureSelenide",
+                new AllureSelenide()
+                        .screenshots(true)
+                        .savePageSource(false));
+    }
+
+    @After
+    public static void driverClose() { WebDriverRunner.closeWebDriver(); }
+
+}
